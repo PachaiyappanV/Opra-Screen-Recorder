4f5736b01c4168c97ded3b04ffc6091d17695d23
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-20 21:05:27 +0530

    feat(app): restructure App component to include QueryClientProvider and additional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import ControlLayout from "./layouts/ControlLayout";
+import AuthButton from "./components/AuthButton";
+import Widget from "./components/Widget";
+
+const client = new QueryClient();
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <QueryClientProvider client={client}>
+      <ControlLayout>
+        <AuthButton />
+        <Widget />
+      </ControlLayout>
+      <Toaster />
+    </QueryClientProvider>
   );
 }
 
