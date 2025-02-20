a92c240736d386d7e9d32c3489c290936f81c6da
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-20 20:53:54 +0530

    feat(utils): add getMediaSources function to retrieve display and audio input devices

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,15 @@ export function cn(...inputs: ClassValue[]) {
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_HOST_URL,
 });
+
+export const getMediaSources = async () => {
+  const displays = await window.ipcRenderer.invoke("getSources");
+
+  const enumerateDevices =
+    await window.navigator.mediaDevices.enumerateDevices();
+  const audioInputs = enumerateDevices.filter(
+    (device) => device.kind === "audioinput"
+  );
+
+  return { displays, audioInputs };
+};
