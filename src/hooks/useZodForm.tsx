90213aa758e4ffa4c57fd00d54df7c69189afe92
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-23 08:55:26 +0530

    feat(hooks): add custom useZodForm hook for form validation with Zod

---
+++ src/hooks/useZodForm.tsx
@@ -0,0 +1,23 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z, { ZodSchema } from "zod";
+
+const useZodForm = (
+  schema: ZodSchema,
+
+  defaultValues?: any
+) => {
+  const {
+    register,
+    watch,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+
+  return { register, watch, reset, handleSubmit, errors };
+};
+export default useZodForm;
