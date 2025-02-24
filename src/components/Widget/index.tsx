5f196f5ed4904dfb9e93fcc66f48cbb7d952d36a
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-24 11:05:18 +0530

    feat(components): integrate MediaConfig component into Widget for user media display

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,9 +1,11 @@
 import { ClerkLoading, SignedIn } from "@clerk/clerk-react";
 import { Loader } from "../Loader";
 import { useProfile } from "@/hooks/useProfile";
+import MediaConfig from "../MediaConfig";
 
 const Widget = () => {
   const { profile, state } = useProfile();
+
   return (
     <div className="p-5">
       <ClerkLoading>
@@ -13,7 +15,7 @@ const Widget = () => {
       </ClerkLoading>
       <SignedIn>
         {profile ? (
-          <h1>Media</h1>
+          <MediaConfig user={profile.user} state={state} />
         ) : (
           <div className="w-full h-full flex justify-center items-center">
             <Loader color="#fff" />
