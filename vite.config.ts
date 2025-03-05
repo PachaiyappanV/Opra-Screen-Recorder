e60cb79d4003b77c7db8d9f7d8f19a0aac2a4deb
PachaiyappanV <pachaiyappan394@gmail.com>
2025-03-05 15:57:39 +0530

    refactor: remove unused favicon link from HTML files and update API target in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/api",
+        target: "https://opra-six.vercel.app/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
