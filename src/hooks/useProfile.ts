deadffacb31f02d70c7949197dd690c2079dc6cf
PachaiyappanV <pachaiyappan394@gmail.com>
2025-03-05 20:36:04 +0530

    feat(hook): enhance useProfile fetching with error handling and loading state

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,34 +1,59 @@
 import { httpClient } from "@/lib/utils";
 import { Profile } from "@/types";
-import { useUser } from "@clerk/clerk-react";
+
 import { useEffect, useState } from "react";
 import { useMediaSources } from "./useMediaSources";
 
 export const useProfile = () => {
+  const [userId, setUserId] = useState<string | null>(
+    window.localStorage.getItem("userId")
+  );
+  console.log("userId", userId);
   const [profile, setProfile] = useState<Profile | null>(null);
   const { state, fetchMediaResources } = useMediaSources();
 
-  const { user } = useUser();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const fetchUserProfile = async (userId: string) => {
-    const response = await httpClient.get(`/auth/${userId}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    try {
+      setIsLoading(true);
+      const response = await httpClient.get(`/auth/${userId}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("response", response);
+      if (response.data.status !== 200) {
+        throw new Error("Failed to fetch user profile");
+      }
 
-    setProfile(response.data);
+      setProfile(response.data);
+      setIsLoading(false);
+      setError(false);
+      window.localStorage.setItem("userId", userId);
+    } catch (error) {
+      setError(true);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-    if (user && user.id) {
-      fetchUserProfile(user.id);
-      fetchMediaResources();
+    fetchMediaResources();
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      fetchUserProfile(userId);
     }
-  }, [user]);
+  }, [userId]);
 
   return {
     profile,
+    setProfile,
     state,
+    setUserId,
+    isLoading,
+    error,
   };
 };
